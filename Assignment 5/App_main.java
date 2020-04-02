f3a117b6fc72995447abeeded4184c0713d32125
RanaAyman <58454300+RanaAyman@users.noreply.github.com>
2020-04-02 08:15:38 +0200

    Modification To Guide User Only.
    I added more print statements only to guide the user.

--- Assignment 5/App_main.java
+++ Assignment 5/App_main.java
@@ -7,7 +7,9 @@ public class App_main {
 	public static void main(String[] args) {
 		ExpressionEvaluator p = new ExpressionEvaluator();
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter a symbolic infix expression to convert it to postfix notation");
+		System.out.println("Enter a symbolic infix expression to convert it to postfix notation & evaluate its value.");
+		System.out.println("Enter the expression with spaces between the numbers \" With parentheses or not ....\" \nFor Example :   (6 / 2 - 3) + (2 * 4)    <<<OR>>>    a * (b + c) * d");
+		System.out.println("NOTE : Expressions with letters will give you postfix notation only and will throw an error message for evaluating the value.");
 		String exp = scan.nextLine();
 		String Postfix = p.infixToPostfix(exp) ;
 		System.out.println("Postfix notation is : ");
